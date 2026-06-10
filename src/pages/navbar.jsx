68807433077c0db3460c1ea4ebec1c8338076500
Hussain Motarwala <motarwala.hussain@gmail.com>
2026-06-10 14:32:29 +0530

    Update navbar.jsx

--- src/pages/navbar.jsx
+++ src/pages/navbar.jsx
@@ -2,7 +2,7 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
-import "./Navbar.css";
+import "./navbar.css";
 
 function Navbar() {
   const { user, setUser } = useContext(AuthContext);
